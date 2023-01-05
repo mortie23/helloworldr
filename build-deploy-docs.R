5537e57fb1c28df9eea66358788429cece0762f8
Michael Jordan <michael.jordan@mortimer.xyz>
2023-01-05 14:08:06 -0800

    feat: :rocket: second deploy explicit id

--- build-deploy-docs.R
+++ build-deploy-docs.R
@@ -1,8 +1,9 @@
 pkgdown::build_site()
 
-# appId = '',
+
 rsconnect::deployApp(
   appDir = "docs/",
+  appId = 'b03ab3e0-cd65-4e79-baaa-48103a828e9a',
   appPrimaryDoc = "index.html",
   forceUpdate = TRUE
 )
